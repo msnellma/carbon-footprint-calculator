091a0993d74773e0e092e7b5edae4d70572c4a7a
msnellma <markus.snellman@capgemini.com>
2024-10-14 13:48:13 +0200

    Add backend project structure

---
+++ backend/src/main/java/com/poc/carbon_footprint_calculator/CarbonFootprintCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.poc.carbon_footprint_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarbonFootprintCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarbonFootprintCalculatorApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/poc/carbon_footprint_calculator/CarbonFootprintCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.carbon_footprint_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarbonFootprintCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
